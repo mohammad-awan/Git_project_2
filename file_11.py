18a7979856bcd2914f12d05bffde3a6745f24c99
muhammad-awan <nameismohammad@gmail.com>
2025-10-01 18:14:54 +0500

    first commit all file to repositroy

---
+++ file_11.py
@@ -0,0 +1,7 @@
+list_1 = [5, 4, 3, 7, 8, 9]
+sum_of_list = 0
+
+for i in list_1:
+    sum_of_list = sum_of_list + i
+
+print(sum_of_list)
---
+++ file_22.py
@@ -0,0 +1,9 @@
+list_2 = [1, 2, 3, 4, 5]
+
+greater = 0
+
+for i in list_2:
+    if i > greater:
+        greater = i
+
+print("Greater no:", greater)
