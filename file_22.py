4cf9cb53a416b6810c7e35faf1f52699ebc651b9
muhammad-awan <nameismohammad@gmail.com>
2025-10-01 18:24:07 +0500

    Change the Logic of loop

--- file_22.py
+++ file_22.py
@@ -2,8 +2,8 @@ list_2 = [1, 2, 3, 4, 5]
 
 greater = 0
 
-for i in list_2:
-    if i > greater:
-        greater = i
+for item in list_2:
+    if item > greater:
+        greater = item
 
 print("Greater no:", greater)
